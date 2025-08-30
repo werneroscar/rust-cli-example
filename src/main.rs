02f85874fc4411295c1ecc5511a776d3b557eb51
werneroscar <wernertsekumah@gmail.com>
2025-08-30 13:27:09 +0000

    using clap to build cli tool, added lib.rs for organization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::App;
+use serde_json;
+use blkrs::run_lsblk;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("lsblk")
+        .version("0.0.1")
+        .author("Werner Oscar")
+        .about("lsblk in Rust")
+        .arg(
+            clap::Arg::with_name("device")
+                .help("Device to query")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    let device = matches.value_of("device").unwrap();
+    let output = serde_json::to_string_pretty(&run_lsblk(device)).unwrap();
+    println!("{}", output);
 }
