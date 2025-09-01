dae8e35c17fa5352b521296066d8c60e26f1a857
werneroscar <wernertsekumah@gmail.com>
2025-09-01 20:24:53 +0000

    clean implementation of Clap CLI framework in main.rs, removed utils.rs mods file removed serde-json from local

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,22 @@
-use clap::{Command, Arg};
+use clap::Parser;
 use serde_json;
 use blkrs::run_lsblk;
-mod util; // pull util module into scope
+
+#[derive(Parser)]
+#[command(
+    version = "0.0.1",
+    author = "Werner Oscar",
+    name = "lsblk-rs",
+    about = "lsblk in Rust"
+)]
+
+struct Opts {
+    #[clap(help = "Device to query")]
+    device: String,
+}
 
 fn main() {
-    let matches = Command::new("lsblk")
-        .version("0.0.1")
-        .author("Werner Oscar")
-        .about("lsblk in Rust")
-        .arg(
-            Arg::new("device")
-                .help("Device to query")
-                .required(true)
-                .index(1),
-        )
-        .get_matches();
-
-    if let Some(device) = matches.get_one::<String>("device") {
-        let output = serde_json::to_string_pretty(&run_lsblk(device)).unwrap();
-        println!("{}", output);
-    } else {
-        println!("No device provided");
-    }
+    let opts: Opts = Opts::parse();
+    let output = serde_json::to_string_pretty(&run_lsblk(&opts.device)).unwrap();
+    println!("Opts: {output}");
 }
