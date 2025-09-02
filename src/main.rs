e77133585f509054f034ef5a4ef48c34435fce32
werneroscar <wernertsekumah@gmail.com>
2025-09-02 15:22:55 +0000

    added logging using the env and env_logging crates. added logging to file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::fs::OpenOptions;
+
 use clap::{Parser, ArgAction};
 use serde_json;
 use blkrs::run_lsblk;
+use log::{Level, LevelFilter};
+use env_logger::{Builder, Target};
 
 
 #[derive(Parser)]
@@ -9,6 +13,9 @@ use blkrs::run_lsblk;
 struct Opts {
     #[clap(short, long, action = ArgAction::Count, help = "Set verbosity level")]
     verbose_level: u8,
+
+    #[clap(long, help = "Enable logging to a file", env = "BLKRS_LOG_FILE")]
+    log_file: bool,
 
     #[clap(short, long, help = "Enable debug mode", env = "BLKRS_DEBUG")]
     debug: bool,
@@ -31,9 +38,37 @@ struct InfoOpts {
 
 fn main() {
     let opts: Opts = Opts::parse();
+
+    let mut builder = Builder::new();
+    builder.filter_level(LevelFilter::Debug);
+
+    if opts.log_file {
+        let file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("blkrs.log")
+            .unwrap();
+        builder.target(Target::Pipe(Box::new(file)));
+    } else {
+        builder.target(Target::Stderr);
+    }
+
+    builder.init();
     
     // Example of using global args
     if opts.debug {
-        println!("Debug mode is enabled");
+        log::info!("Debug mode is enabled");
+    }
+
+    log::debug!("debug message logging is enabled");
+    match opts.cmd {
+        Command::Info(info_opts) => {
+            match opts.verbose_level {
+                0 | 1 | 2 => println!("Running in verbose mode level {}", opts.verbose_level),
+                _ => eprintln!("Select a valid verbosity level (0-2)"),
+            }
+            let output = serde_json::to_string_pretty(&run_lsblk(&info_opts.device)).unwrap();
+            println!("{}", output);
+        }
     }
 }
