ec30d60f43d96e034926df31543bd4d230075c4d
werneroscar <wernertsekumah@gmail.com>
2025-09-02 10:42:57 +0000

    enabled "env" feature in clap to enable the use of env vars. added more opts

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
-use clap::Parser;
+use clap::{Parser, ArgAction};
 use serde_json;
 use blkrs::run_lsblk;
 
+
 #[derive(Parser)]
-#[command(
-    version = "0.0.1",
-    author = "Werner Oscar",
-    name = "lsblk-rs",
-    about = "lsblk in Rust"
-)]
+#[command(version = "0.0.1", author = "Werner Oscar", name = "lsblk-rs", about = "lsblk in Rust")]
 
 struct Opts {
-    #[clap(help = "Device to query")]
+    #[clap(short, long, action = ArgAction::Count)]
+    verbose_level: u8,
+
+    #[clap(short, long)]
+    debug: bool,
+    
+    #[clap(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Parser)]
+enum Command {
+    #[clap(name = "info", about = "Get device info")]
+    Info(InfoOpts),
+}
+
+#[derive(Parser)]
+struct InfoOpts {
+    #[clap(help = "Device to get info about")]
     device: String,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
-    let output = serde_json::to_string_pretty(&run_lsblk(&opts.device)).unwrap();
-    println!("Opts: {output}");
+    match opts.cmd {
+        Command::Info(info) => {
+            let device = info.device;
+            let output = run_lsblk(&device);
+            println!("{}", serde_json::to_string_pretty(&output).unwrap());
+        }
+    }
 }
